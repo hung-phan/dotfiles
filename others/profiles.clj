80df2d5747b85a5b90081c58d7b2b814437eb936
hung-phan <colorvisavn@gmail.com>
2017-08-06 00:08:32 +0800

    Update profiles

--- others/profiles.clj
+++ others/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[venantius/ultra "0.5.0"]
+
+{:user {:plugins [[venantius/ultra "0.5.1"]
                   [lein-ancient "0.6.10"]
-                  [lein-kibit "0.1.2"]]}}
+                  [lein-kibit "0.1.5"]]}}
